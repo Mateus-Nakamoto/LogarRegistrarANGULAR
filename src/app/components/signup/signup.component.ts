221d9ca18773b7d9953d428842e8a82de19eddaf
MATEUSNAKAMOTO <DRAKAMZEN@GMAIL.COM>
2023-10-03 13:28:22 -0300

    new file:   .vs/LogarRegistrarANGULAR/v17/.wsuo 	new file:   .vs/VSWorkspaceState.json 	new file:   .vs/slnx.sqlite 	modified:   src/app/app-routing.module.ts 	modified:   src/app/app.component.html 	modified:   src/app/app.module.ts 	new file:   src/app/components/dashboard/dashboard/dashboard.component.css 	new file:   src/app/components/dashboard/dashboard/dashboard.component.html 	new file:   src/app/components/dashboard/dashboard/dashboard.component.spec.ts 	new file:   src/app/components/dashboard/dashboard/dashboard.component.ts 	new file:   src/app/components/login/login.component.css 	new file:   src/app/components/login/login.component.html 	new file:   src/app/components/login/login.component.spec.ts 	new file:   src/app/components/login/login.component.ts 	new file:   src/app/components/signup/signup.component.css 	new file:   src/app/components/signup/signup.component.html 	new file:   src/app/components/signup/signup.component.spec.ts 	new file:   src/app/components/signup/signup.component.ts 	new file:   src/app/helpers/validateform.ts 	new file:   src/app/services/auth.service.spec.ts 	new file:   src/app/services/auth.service.ts 	new file:   src/assets/css/common.css 	modified:   src/index.html 	modified:   src/styles.css

---
+++ src/app/components/signup/signup.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
+import ValidateForm from 'src/app/helpers/validateform';
+import { AuthService } from 'src/app/services/auth.service';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent implements OnInit{
+
+  type: string = "password";
+  isText: boolean = false;
+  eyeIcon: string = "fa-eye-slash";
+  signUpForm!: FormGroup;
+
+constructor( private fb: FormBuilder, private auth: AuthService, private router:Router){}
+
+ngOnInit(): void{
+  this.signUpForm = this.fb.group({
+     pessoaNome: ['', Validators.required],
+     pessoaUsuario: ['', Validators.required],
+     pessoaEmail: ['', Validators.required],
+     pessoaSenha: ['', Validators.required]
+
+  })
+}
+
+hideShowPass(){
+  this.isText = !this.isText;
+  this.isText ? this.eyeIcon = "fa-eye": this.eyeIcon = "fa-eye-slash";
+  this.isText ? this.type = "text" : this.type = "password";
+}
+
+onSignup(){
+  if(this.signUpForm.valid){
+    // perform logic for signup
+    this.auth.signUp(this.signUpForm.value)
+    .subscribe({
+      next:(res=>{
+        alert(res.message)
+        this.signUpForm.reset();
+        this.router.navigate(['login']);
+      })
+      ,error:(err=>{
+        alert(err?.error.message)
+      })
+    })
+
+    console.log(this.signUpForm.value)
+  }else{
+    ValidateForm.validateAllFormsFields(this.signUpForm)
+    // logic to validate signup
+  }
+}
+
+
+
+}
